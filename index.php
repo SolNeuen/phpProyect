630476b5314189b6d68b21a5a45b4c953e774f1b
Soledad Nevendorf <58151020+SolNeuen@users.noreply.github.com>
2021-07-14 17:49:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    
+    session_start();
+
+    require_once "header.php";
+/*     require_once "body.php"; */
+?>
+
+<section>
+<?php 
+    if (isset($_SESSION["userId"])) {
+         echo "<h1>¡Bienvenide " . $_SESSION["userFullName"] ."!</h1>";
+    }
+?>
+</section>
+
+<?php 
+  require_once "main.php";
+  require_once "footer.php";
+?>
+
+
+<script src="js/script.js"></script>
