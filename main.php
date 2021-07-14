a519507235a96d1cf449395744d67a0fabab118e
Soledad Nevendorf <58151020+SolNeuen@users.noreply.github.com>
2021-07-14 17:50:12 -0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,58 @@
+<section class="main">
+    <!-- comandancia flota estelar -->
+    <article>
+        <figure>
+            <img src="img/starfleet-thumbnail.png">
+        </figure>
+        <ul class="article">
+            <li class="article">
+                <h4>Comandancia</h4>
+            </li>
+            <li class="article">
+                <strong>La Flota Estelar</strong> es el servicio de exploración y defensa del espacio profundo mantenido por la <strong>Federación Unida de Planetas</strong>. Sus funciones principales incluyen el avance del conocimiento de la Federación sobre la galaxia y sus habitantes.</p>
+            </li>
+            <li class="article">
+                <button class="article">Conocer más</button>
+            </li>
+        </ul>
+    </article>
+
+    <!-- tu lugar en la flota -->
+    <article>
+        <figure>
+            <img src="img/enterpriseCrew.jpg">
+        </figure>
+        <ul class="article">
+            <li class="article">
+                <h4>Tripulación</h4>
+            </li>
+            <li class="article">
+                <strong>Les tripulantes</strong> tienen el deber de llevar adelante la misión de cada Nave de la Flota. Hay muchas carreras que puedes elegir para servir en la flota: comandancia, medicina, seguridad, ingeniería, etc. </p>
+            </li>
+            <li class="article">
+                <button class="article"><a href="theCrew.php">Conocer más</a></button>
+            </li>
+        </ul>
+    </article>
+    <!-- flota estelar -->
+    <article>
+        <figure>
+            <img src="img/starFleetShips.png">
+        </figure>
+        <ul class="article">
+            <li class="article">
+                <h4>Flota Estelar</h4>
+            </li>
+            <li class="article">
+                La columna vertebral de las operaciones de la Flota Estelar. Estos buques, todos con <strong>capacidad warp</strong>, son capaces de llevar a cabo todos los mandatos del servicio: exploración, defensa, transporte de suministros y recursos y atención médica. </p>
+            </li>
+            <li class="article">
+                <button class="article">Conocer más</button>
+            </li>
+        </ul>
+    </article>
+</section>
+
+<?php 
+  require_once "footer.php";
+?>
